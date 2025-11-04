8ce1316206a58f81f669849fb02ce0209c784c4c
MaxoGitAcc <maxowork19@gmail.com>
2025-11-04 13:57:18 +0400

    Step 9 Added Components.js

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,22 +7,6 @@ sap.ui.define([
     "use strict";
  
     return Controller.extend("ui5.walkthrough.controller.App", {
-      onInit() {
-          const oData = {
-             recipient : {
-                name : "World"
-             }
-          };
-          const oModel = new JSONModel(oData);
-          this.getView().setModel(oModel);
-
-
-          const i18nModel = new ResourceModel({
-             bundleName: "ui5.walkthrough.i18n.i18n"
-          });
-          this.getView().setModel(i18nModel, "i18n");
-       },
- 
        onShowHello() {
           const oBundle = this.getView().getModel("i18n").getResourceBundle();
           const sRecipient = this.getView().getModel().getProperty("/recipient/name");
