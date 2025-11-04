56f5d95c71d79279a3ffe1fe39a4d2c0a4be8cc1
MaxoGitAcc <maxowork19@gmail.com>
2025-11-04 13:39:13 +0400

    Step 8 Added language settings

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,23 +1,34 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], (Controller, MessageToast, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+ ], (Controller, MessageToast, JSONModel, ResourceModel) => {
     "use strict";
-
+ 
     return Controller.extend("ui5.walkthrough.controller.App", {
-        onInit() {
-            const oData = {
-                recipient : {
-                    name : "World"
-                }
-            };
-            const oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-        },
+      onInit() {
+          const oData = {
+             recipient : {
+                name : "World"
+             }
+          };
+          const oModel = new JSONModel(oData);
+          this.getView().setModel(oModel);
 
-        onShowHello() {
-            MessageToast.show("Hello World!");
-        }
-    })
-})
+
+          const i18nModel = new ResourceModel({
+             bundleName: "ui5.walkthrough.i18n.i18n"
+          });
+          this.getView().setModel(i18nModel, "i18n");
+       },
+ 
+       onShowHello() {
+          const oBundle = this.getView().getModel("i18n").getResourceBundle();
+          const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+          const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+ 
+          MessageToast.show(sMsg);
+       }
+    });
+ });
