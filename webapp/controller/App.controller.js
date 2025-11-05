33d86f78294fb2198fade3cccc383c6efbd9be2b
MaxoGitAcc <maxowork19@gmail.com>
2025-11-05 16:36:22 +0400

    Step 15 moved panel

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,18 +1,7 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
- ], (Controller, MessageToast, JSONModel, ResourceModel) => {
+ ], (Controller) => {
     "use strict";
  
-    return Controller.extend("ui5.walkthrough.controller.App", {
-       onShowHello() {
-          const oBundle = this.getView().getModel("i18n").getResourceBundle();
-          const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-          const sMsg = oBundle.getText("helloMsg", [sRecipient]);
- 
-          MessageToast.show(sMsg);
-       }
-    });
- });
+    return Controller.extend("ui5.walkthrough.controller.App", {})
+});
