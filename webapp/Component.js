5118c225242238b29eb58bf7cc02046b99d1f010
MaxoGitAcc <maxowork19@gmail.com>
2025-11-07 16:10:54 +0400

    Step 36 denisty

--- webapp/Component.js
+++ webapp/Component.js
@@ -27,7 +27,11 @@ sap.ui.define([
 			 this.setModel(oDeviceModel, "device");
 
           this.getRouter().initialize();
-       }
+       },
+
+       getContentDensityClass() {
+			return Device.support.touch ? "sapUiSizeCozy" : "sapUiSizeCompact";
+		}
     });
  });
  
